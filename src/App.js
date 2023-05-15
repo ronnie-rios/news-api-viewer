14cbbc6f353ac785b69237b39f61047ae3233c88
Ronnie Rios <ronniesmail14@gmail.com>
2023-05-14 19:17:57 -0500

    react router dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-
+import { BroswerRouter as Router, Routes, Route } from 'react-router-dom';
+import TopNewsPage from './pages/TopNewsPage';
+import CategoriesPage from './pages/CategoriesPage';
+import Search from './pages/Search';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
-
+      <Header />
+      <Router>
+        <Routes>
+          <Route path='/' element={<TopNewsPage />}/>
+          <Route path='/' element={<CategoriesPage />}/>
+          <Route path='/' element={<Search />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
