5d03f18feb609db409c22d76253622743d0957e8
Ronnie Rios <ronniesmail14@gmail.com>
2023-05-15 16:38:27 -0500

    replaced daisyui with bootstrap

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,33 @@
-import { Link } from 'react-router-dom';
-import { useCountry } from '../context/countrycontext'
+import { Link } from "react-router-dom";
+import { useCountry } from "../context/countrycontext";
 
 const Header = () => {
   const { country, toggleCountry } = useCountry();
 
-
   return (
-    <nav>
-      <Link to='/'>Top News</Link>
-      <Link to='/categories'>Categories</Link>
-      <Link to='/search'>Search</Link>
-      <button onClick={toggleCountry}>{country === "us" ? "View Great Britain News" : "View US News"}</button>
+    <nav className="navbar bg-base-100">
+      <div className="flex-1">
+        <button className="btn btn-ghost normal-case text-xl">
+          <Link to="/">Top News</Link>
+        </button>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link to="/categories">Categories</Link>
+          </li>
+          <li>
+            <Link to="/search">Search</Link>
+          </li>
+          <li>
+            <button onClick={toggleCountry}>
+              {country === "us" ? "View Great Britain News" : "View US News"}
+            </button>
+          </li>
+        </ul>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
