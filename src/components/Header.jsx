05372db3cb39ed7a89664c78fcf90b3baf686c6a
Ronnie Rios <ronniesmail14@gmail.com>
2023-05-15 17:42:55 -0500

    navbar stylign

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,25 +5,23 @@ const Header = () => {
   const { country, toggleCountry } = useCountry();
 
   return (
-    <nav className="navbar bg-base-100">
-      <div className="flex-1">
+    <nav className="p-4 sticky w-full top-0 left-0 right-0">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <button className="btn btn-ghost normal-case text-xl">
           <Link to="/">Top News</Link>
         </button>
-      </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <Link to="/categories">Categories</Link>
-          </li>
-          <li>
-            <Link to="/search">Search</Link>
-          </li>
-          <li>
-            <button onClick={toggleCountry}>
-              {country === "us" ? "View Great Britain News" : "View US News"}
-            </button>
-          </li>
+        <li className="block pl-3 pr-4">
+          <button onClick={toggleCountry} className="bg-gray-200 p-4">
+            {country === "us" ? "View Great Britain News" : "View US News"}
+          </button>
+        </li>
+        <ul className="flex flex-row font-medium">
+          <Link to="/categories">
+            <li className="block pl-3 pr-4">Categories</li>
+          </Link>
+          <Link to="/search">
+            <li className="block pl-3 pr-4">Search</li>
+          </Link>
         </ul>
       </div>
     </nav>
